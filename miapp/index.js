bcf7570a13c1688f13672b41005fc9943edaa63e
Nicolas Karlezi <nicolaskarlezi@MacBook-Pro-de-Nicolas.local>
2021-10-15 11:57:42 -0300

    primer push

---
+++ miapp/index.js
@@ -0,0 +1,28 @@
+/* eslint no-console: "off" */
+
+const db = require('./src/models');
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/src/views/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
